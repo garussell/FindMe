d03e7746f25f421d88cdec7e7b23159b5de97e5b
Allen Russell <garyrusselljr@gmail.com>
2026-03-17 21:20:03 -0600

    Initial Commit

---
+++ FindMeUITests/FindMeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FindMeUITestsLaunchTests.swift
+//  FindMeUITests
+//
+//  Created by Allen Russell on 3/17/26.
+//
+
+import XCTest
+
+final class FindMeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
